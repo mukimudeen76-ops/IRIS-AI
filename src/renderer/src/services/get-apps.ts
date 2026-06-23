32a2ccb395db8b8b45e1b4978eebe398b0f7df64
Harsh Pandey <160850571+201Harsh@users.noreply.github.com>
2026-06-24 01:48:57 +0530

    chore: remove unused file and associated references

--- src/renderer/src/services/get-apps.ts
+++
@@ -1,7 +0,0 @@
-export const getRunningApps = async (): Promise<string[]> => {
-  try {
-    return await window.electron.ipcRenderer.invoke('get-running-apps')
-  } catch (err) {
-    return []
-  }
-}
