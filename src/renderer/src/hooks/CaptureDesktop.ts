28c0b68b9ab22b4ebaaadf31ea7cedfbcbfb4434
Harsh Pandey <160850571+201Harsh@users.noreply.github.com>
2026-06-24 01:58:59 +0530

    chore: delete CaptureDesktop.ts

--- src/renderer/src/hooks/CaptureDesktop.ts
+++
@@ -1,7 +0,0 @@
-export const getScreenSourceId = async (): Promise<string | null> => {
-  try {
-    return await window.electron.ipcRenderer.invoke('get-screen-source')
-  } catch (err) {
-    return null
-  }
-}
