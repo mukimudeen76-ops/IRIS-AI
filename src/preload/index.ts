7fd408f46220929b8b3e5d72f95dc9b458718f8b
Harsh Pandey <160850571+201Harsh@users.noreply.github.com>
2026-06-24 01:41:41 +0530

    feat: add preload script with context bridge for research process communication

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -3,43 +3,6 @@ import { electronAPI } from '@electron-toolkit/preload'
 
 const api = {}
 
-const irisAPI = {
-  startSession: () => {
-    ipcRenderer.send('iris:start-session')
-  },
-
-  getHistory: () => ipcRenderer.invoke('iris:get-history'),
-  stopSession: () => ipcRenderer.send('iris:stop-session'),
-  toggleMic: (isMuted: boolean) => ipcRenderer.send('iris:toggle-mic', isMuted),
-
-  onSystemStatus: (callback: (status: string) => void) => {
-    ipcRenderer.on('iris:system-status', (_event, status) => callback(status))
-  },
-
-  onSpeakingState: (callback: (isSpeaking: boolean) => void) => {
-    ipcRenderer.on('iris:speaking-state', (_event, isSpeaking) => callback(isSpeaking))
-  },
-
-  onTranscript: (callback: (data: any) => void) => {
-    ipcRenderer.removeAllListeners('iris:transcript')
-    ipcRenderer.on('iris:transcript', (_event, data) => callback(data))
-  },
-
-  onTranscriptComplete: (callback: () => void) => {
-    ipcRenderer.removeAllListeners('iris:transcript-complete')
-    ipcRenderer.on('iris:transcript-complete', () => callback())
-  },
-
-  sendVisionFrame: (base64Frame: string) => ipcRenderer.send('iris:send-vision-frame', base64Frame),
-
-  removeAllListeners: () => {
-    ipcRenderer.removeAllListeners('iris:system-status')
-    ipcRenderer.removeAllListeners('iris:speaking-state')
-    ipcRenderer.removeAllListeners('iris:transcript')
-    ipcRenderer.removeAllListeners('iris:transcript-complete')
-  }
-}
-
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', {
@@ -51,7 +14,6 @@ if (process.contextIsolated) {
     })
     contextBridge.exposeInMainWorld('api', api)
 
-    contextBridge.exposeInMainWorld('iris', irisAPI)
 
     contextBridge.exposeInMainWorld('irisAgent', {
       startResearch: (query: string) => ipcRenderer.invoke('trigger-deep-research', { query }),
